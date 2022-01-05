ca7f4151fac39c0e007ad2f1674edf0a8dcbf8c9
matprogrammer <matprogrammer@gmail.com>
2022-01-05 20:37:51 -0300

    initial commit

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const dbConfig = require("../db/db.config");
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: 0,
+  dialectOptions: {
+    socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+  },
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.tutorials = require("./tutorial")(sequelize, Sequelize);
+
+module.exports = db;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const db = require("./models");
+db.sequelize.sync();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+require("./routes/tutorialsRoutes")(app);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
